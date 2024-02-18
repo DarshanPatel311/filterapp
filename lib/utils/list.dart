3c9b9670a1ebbcd7350490fb5bb5acf538ea7b82
~JAY GOGA~ <dkpatel3116@gmail.com>
2024-02-18 19:09:40 +0530

    Initial commit

---
+++ lib/utils/list.dart
@@ -0,0 +1,81 @@
+List productData1 = [
+  {
+    'id' : 1,
+    'name' : 'iphone 13 pro',
+    'brand' : 'Apple',
+    'price' : 999.9,
+
+  },
+  {
+    'id' : 2,
+    'name' : 'iphone 13 pro max',
+    'brand' : 'Apple',
+
+    'price' : 809,
+
+  },
+  {
+    'id' : 3,
+    'name' : 'iphone 14 ',
+    'brand' : 'Apple',
+
+    'price' : 1505.9,
+
+  },
+  {
+    'id' : 4,
+    'name' : 'iphone 14 pro max',
+    'brand' : 'Apple',
+
+    'price' : 914.3,
+
+  },
+  {
+    'id' : 5,
+    'name' : 'iphone 16 pro',
+    'brand' : 'Apple',
+
+    'price' : 999.7,
+
+  },
+  {
+    'id' : 6,
+    'name' : 'iphone 11 pro',
+    'brand' : 'Apple',
+
+    'price' : 649.3,
+
+  },
+  {
+    'id' : 7,
+    'name' : 'iphone 12 pro max',
+    'brand' : 'Apple',
+
+    'price' : 685.5,
+
+  },
+  {
+    'id' : 8,
+    'name' : 'iphone xs max',
+    'brand' : 'Apple',
+
+    'price' : 155.7,
+
+  },
+  {
+    'id' : 9,
+    'name' : 'iphone 15 pro max',
+    'brand' : 'HP',
+
+    'price' : 1000,
+
+  },
+  {
+    'id' : 10,
+    'name' : 'iphone 12 pro',
+    'brand' : 'Apple',
+    'price' : 381.16,
+
+  },
+
+];
